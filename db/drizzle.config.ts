c4d13ef30f6f017ee4b957734b1abe47904266aa
Arthur Conrozier <arthur.conrozier@student-cs.fr>
2025-05-24 12:16:48 +0200

    test

--- db/drizzle.config.ts
+++ db/drizzle.config.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: "./db/migrations",
-  schema: "./db/schema/index.ts",
+  out: "./migrations",
+  schema: "./schema/index.ts",
   dialect: "mysql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
