1ee94a1159bd91ca50957c0ed595bdf9a1947167
Arthur Conrozier <arthur.conrozier@student-cs.fr>
2025-12-01 09:43:13 +0100

    lint

--- app/page.tsx
+++ app/page.tsx
@@ -13,29 +13,18 @@ export default async function Home() {
         <Card className="bg-white/10 backdrop-blur-sm p-8 mb-[-20] max-w-5xl mx-auto border border-white/20 z-10 relative shadow-lg shadow-black/30">
           <div className="grid md:grid-cols-2 gap-8 text-lg leading-relaxed text-white">
             <p className="text-justify">
-              Here you'll find projects created in the GameLab, an experimental 
-              English course offered at CentraleSupélec. It's one of several 
+              Here you&apos;ll find projects created in the GameLab, an experimental 
+              English course offered at CentraleSupélec. It&apos;s one of several 
               electives available in your first and second years.
             </p>
             <p className="text-justify">
               The course gives you space to invent, pitch, experiment with, and 
-              test your game, completing a finished project by semester's end. 
+              test your game, completing a finished project by semester&apos;s end. 
               Computer games, card games, learning apps, and physical games all fall within scope.
               Feel free to download, test, and comment.
             </p>
           </div>
         </Card>
-
-        /*<Card className="bg-white/10 backdrop-blur-sm p-8 mb-16 max-w-5xl mx-auto border border-white/20 z-0 relative">
-          <div className="grid md:grid-cols-2 gap-8 text-lg leading-relaxed text-white">
-            <p className="mb-2 text-justify">
-              Computer games, printable card games, tracking and learning apps,
-              and physical games all fall within the scope of the GameLab.
-            </p>
-
-            <p>Feel free to download, test, and comment on the games.</p>
-          </div>
-        </Card>*/
 
         <h4 className="text-xl font-semibold mb-12 w-full text-center">
           See you in the Lab.
